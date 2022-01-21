61aa256af5a342ef22c4e2ce556c4976a49ef374
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2022-01-21 19:08:08 +0300

    Update Button.js

--- src/components/Js/Button.js
+++ src/components/Js/Button.js
@@ -3,7 +3,7 @@ import '../Css/Button.css';
 const Button = () => {
 
     return <div>
-        <button className='button'>Найти</button>
+        <button type='submit'  className='button'>Найти</button>
     </div>
 };
 export default Button;
