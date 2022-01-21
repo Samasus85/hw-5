b020264450f237a1f623d1ab6e809b0c234881ed
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2022-01-21 19:09:49 +0300

    Update Text.js

--- src/components/Js/Text.js
+++ src/components/Js/Text.js
@@ -6,16 +6,13 @@ function Text() {
     const changeHandle = (event) => {
         setValue(event.target.value)
     };
-
     return (
         <div className='text'>
             <textarea placeholder='Ваш комментарий' value={value} onChange={changeHandle} />
             <div className='text__p'>
                 <p >{value}</p>
-
             </div>
         </div>
     )
-}
-
+};
 export default Text;
